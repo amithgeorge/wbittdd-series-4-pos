8cf5426e15b692e5db47600e58a8fd3124f85476
Amith George <strider.am@gmail.com>
2021-12-07 21:21:17 +0530

    refactored to functions for creating test doubles

--- test/com/amithgeorge/pos_outside_in/scan_one_item_test.clj
+++ test/com/amithgeorge/pos_outside_in/scan_one_item_test.clj
@@ -5,16 +5,32 @@
             [com.amithgeorge.pos-outside-in.catalogue :as catalogue]
             [com.amithgeorge.pos-outside-in.display :as display]))
 
+(defn- hashmap->specs
+  [m]
+  (->> m
+       (reduce (fn [acc [k v]] (concat acc [[k] v]))
+               [])
+       vec))
+
+(defn- stub-catalogue
+  [items]
+  (let [specs (hashmap->specs items)]
+    (f/reify-fake
+     catalogue/Catalogue
+     (price :fake specs))))
+
+(defn- mock-display
+  []
+  (f/reify-fake
+   display/Display
+   (price :recorded-fake)))
+
 (deftest item-found
   (testing "Given a barcode for an existing item, it should display its price"
     (f/with-fakes
       (let [irrelevant-price {:amount 10.0M}
             irrelevant-code "some product barcode"
-            catalogue (f/reify-fake
-                       catalogue/Catalogue
-                       (price :fake [[irrelevant-code] irrelevant-price]))
-            display (f/reify-fake
-                     display/Display
-                     (price :recorded-fake))]
+            catalogue (stub-catalogue {irrelevant-code irrelevant-price})
+            display (mock-display)]
         (sut/scan catalogue display irrelevant-code)
         (is (f/method-was-called-once display/price display [irrelevant-price]))))))
