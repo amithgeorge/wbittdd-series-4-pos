a2e3f732cc83c904fd4b4511c3f2718762854f0a
Amith George <strider.am@gmail.com>
2021-12-09 12:52:28 +0530

    tests for ConsoleDisplay to print error messages

---
+++ test/com/amithgeorge/pos_outside_in/integrated/display_message_to_console_test.clj
@@ -0,0 +1,24 @@
+(ns com.amithgeorge.pos-outside-in.integrated.display-message-to-console-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [com.amithgeorge.pos-outside-in.display :as display]
+            [clojure.string :as str]))
+
+(deftest display-not-found-error-message
+  (testing "accept no arguments, prints not found message to stdout"
+    (let [display (reify display/Display
+                    (not-found [_]
+                      (println "Product not found.")))
+          std-out-lines (str/split-lines
+                         (with-out-str
+                           (display/not-found display)))]
+      (is (= ["Product not found."] std-out-lines)))))
+
+(deftest display-invalid-code-error-message
+  (testing "accept no arguments, prints invalid code message to stdout"
+    (let [display (reify display/Display
+                    (invalid-code [_]
+                      (println "Scanning error. Invalid code.")))
+          std-out-lines (str/split-lines
+                         (with-out-str
+                           (display/invalid-code display)))]
+      (is (= ["Scanning error. Invalid code."] std-out-lines)))))
